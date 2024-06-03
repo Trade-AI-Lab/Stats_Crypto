f275b8809b08f9faab5cb41a508660ee06c4154a
Dante-Berth <bertinalexAI@protonmail.com>
2024-06-03 08:31:09 +0200

    [UPDATE] stats

--- src/utils/utils_plotly.py
+++ src/utils/utils_plotly.py
@@ -3,7 +3,8 @@ import plotly.graph_objects as go
 import numpy as np
 from typing import Union
 
-def multiple_signals_same_graph(X:Union[list,np.array],dict_Y:dict):
+
+def multiple_signals_same_graph(X: Union[list, np.array], dict_Y: dict):
     """
     Plot multiple signals on a same graph
     Args:
@@ -16,13 +17,11 @@ def multiple_signals_same_graph(X:Union[list,np.array],dict_Y:dict):
 
     fig = go.Figure()
     for keys in list(dict_Y):
-        fig.add_trace(go.Scatter(x=X,
-                             y=dict_Y[keys],
-                             mode='lines',
-                             name=str(keys)))
+        fig.add_trace(go.Scatter(x=X, y=dict_Y[keys], mode="lines", name=str(keys)))
     fig.show()
 
-def subplots_plotly(X:Union[list,np.array],dict_Y:dict):
+
+def subplots_plotly(X: Union[list, np.array], dict_Y: dict, browser=None):
     """
     Plotting multiple signals on different graph with the same X axis
 
@@ -35,12 +34,18 @@ def subplots_plotly(X:Union[list,np.array],dict_Y:dict):
     """
 
     # Create a subplot grid
-    fig = sp.make_subplots(rows=len(dict_Y), cols=1, shared_xaxes=True, vertical_spacing=0.05)
+    fig = sp.make_subplots(
+        rows=len(dict_Y), cols=1, shared_xaxes=True, vertical_spacing=0.05
+    )
 
     n = 1
     for keys in list(dict_Y):
-        fig.add_trace(go.Scatter(x=X, y=dict_Y[keys], mode='lines', name=keys, showlegend=False), row=n , col=1)
-        n+=1
+        fig.add_trace(
+            go.Scatter(x=X, y=dict_Y[keys], mode="lines", name=keys, showlegend=False),
+            row=n,
+            col=1,
+        )
+        n += 1
 
     # Update the layout
     fig.update_layout(
@@ -50,32 +55,35 @@ def subplots_plotly(X:Union[list,np.array],dict_Y:dict):
     )
 
     # Show the interactive plot
-    fig.show()
-def subplots_plotly_markers(X,dictionnary):
+    fig.show("browser") if browser else fig.show()
+
+
+def subplots_plotly_markers(X, dictionnary):
     import plotly.graph_objects as go
-    fig = sp.make_subplots(rows=len(dictionnary), cols=1, shared_xaxes=True, vertical_spacing=0.05)
+
+    fig = sp.make_subplots(
+        rows=len(dictionnary), cols=1, shared_xaxes=True, vertical_spacing=0.05
+    )
     n = 1
     for key in dictionnary.keys():
         signal = dictionnary[key]["signal"]
         markers = dictionnary[key]["markers"]
 
-        line_signal = go.Scatter(x=X,
-            y=signal,
-            name=f'{key}_signal',
-            mode="lines")
+        line_signal = go.Scatter(x=X, y=signal, name=f"{key}_signal", mode="lines")
         if markers is not None:
             marker_signal = go.Scatter(
                 x=X,
                 y=signal,
-                name=f'{key}_markers',
+                name=f"{key}_markers",
                 marker=dict(
                     color=markers,
                     colorscale=[[0, "red"], [0.5, "yellow"], [1, "green"]],
-                    size=8
+                    size=8,
                 ),
                 mode="markers",
                 customdata=markers,
-                hovertemplate=" highlight_signal= %{customdata} <br> %{y} <br> %{x}")
+                hovertemplate=" highlight_signal= %{customdata} <br> %{y} <br> %{x}",
+            )
             fig.add_trace(marker_signal, row=n, col=1)
         fig.add_trace(line_signal, row=n, col=1)
 
@@ -90,18 +98,18 @@ def subplots_plotly_markers(X,dictionnary):
 
         # Show the interactive plot
     fig.show()
-def threed_plot(X,Y,Z,x_title,y_title,z_title):
+
+
+def threed_plot(X, Y, Z, x_title, y_title, z_title):
     fig = go.Figure(data=[go.Surface(x=X, y=Y, z=Z)])
     fig.update_layout(
-        title='3D Surface Plot',
+        title="3D Surface Plot",
         scene=dict(
             xaxis=dict(title=x_title),
             yaxis=dict(title=y_title),
-            zaxis=dict(title=z_title)
-        )
+            zaxis=dict(title=z_title),
+        ),
     )
 
     # Show plot
     fig.show()
-
-
--- stats.py
+++ stats.py
@@ -4,8 +4,13 @@ absolute_path = os.path.abspath(__file__)[
     : os.path.abspath(__file__).find("Stats_Crypto") + len("Stats_Crypto")
 ]
 sys.path.append(absolute_path)
-from src.utils.utils_df import open_dataframe
+from src.utils.utils_df import open_dataframe, filter_dataframe_by_date_range
 
 PATH = r"data\ETHUSDT-1m-without-aggr.zip"
-df = open_dataframe(PATH=PATH).drop("unnamed: 0", axis=1)
+df = filter_dataframe_by_date_range(
+    open_dataframe(PATH=PATH).drop("unnamed: 0", axis=1),
+    begin_date="2023-04-03",
+    end_date="2023-05-06",
+)
+
 print(df.head())
